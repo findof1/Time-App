bfe63480d18a7402df838b20e1b0a9904264c23e
Findof <joeargon21@gmail.com>
2023-11-01 23:05:43 -0400

    New Update With Css

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const date = document.querySelector(".displayDate")
+
+
+function updateClock(){
+const date = document.querySelector(".displayDate");
 let temp = "Date: ";
 temp += dayjs().format('M/D/YYYY');
-
 date.innerHTML = temp;
-const time = document.querySelector(".displayTime")
+
+const time = document.querySelector(".displayTime");
 temp = "Time: ";
-temp += dayjs().format('hh:mm:ss  A')
+temp += dayjs().format('hh:mm:ss  A');
 time.innerHTML = temp;
 
+const DTNY = document.querySelector(".displayNewYear");
+temp = "Days Until New Years: ";
+temp += 
+DTNY.innerHTML = temp;
+setTimeout(updateClock, 1000);
+}
+updateClock();
